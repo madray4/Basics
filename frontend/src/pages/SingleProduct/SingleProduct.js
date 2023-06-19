f2707cea3d91f64f5e48dff8b67ca5e76b47d228
Henry Chu <henry.chu013198@gmail.com>
2023-06-19 13:02:13 -0400

    Created add item to cart function on frontend and printed to console

--- frontend/src/pages/SingleProduct/SingleProduct.js
+++ frontend/src/pages/SingleProduct/SingleProduct.js
@@ -1,20 +1,32 @@
 import './SingleProduct.css'
 import { useParams, useNavigate, Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const SingleProduct = () => {
-  const { PID } = useParams();
-  const { products } = useSelector(state => state.product);
-  const [selectedSize, setSelectedSize ] = useState(""); 
-
-  const currentProduct = products.filter(product => PID === product._id)[0];
-  const similarProducts = products.filter(product => currentProduct.name === product.name);
   const navigate = useNavigate();
+  const { PID } = useParams();
 
+  const { products } = useSelector(state => state.product);
+  const currentProduct = products.filter(product => PID === product._id)[0];
+  const similarProducts = currentProduct ? products.filter(product => currentProduct.name === product.name) : "";
+  const [selectedSize, setSelectedSize ] = useState(currentProduct ? currentProduct.sizes[0] : ""); 
+
+  // redirects to all products if a use attempts to access a product that doesn't exist
   if(products && !currentProduct){
     navigate("/products/all");
   }
+
+  // automatically selects a size if going directly to product page
+  if(currentProduct && selectedSize === ""){
+    setSelectedSize(currentProduct.sizes[0]);
+  }
+  // functions
+
+  const addItemToCart = () => {
+    console.log(currentProduct);
+    console.log(selectedSize);
+  };
 
   return (
     <div className="single-product-page">
@@ -37,11 +49,15 @@ const SingleProduct = () => {
           </div>
           <div className="single-product-size-wrapper">
             {currentProduct.sizes.map(size => {
-              return <p className={selectedSize === size ? "current" : "not-current"}
+              return <p key={size} className={selectedSize === size ? "current" : "not-current"}
                         onClick={() => setSelectedSize(size)}
                       >{size}</p>
             })}
           </div>
+          <button className="add-to-cart-button" onClick={addItemToCart}>
+            <span className="material-symbols-outlined">shopping_basket</span>
+            Add to Cart
+          </button>
           <p>{currentProduct.description}</p>
         </div>
       </div>
