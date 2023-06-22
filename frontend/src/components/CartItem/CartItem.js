163fd55bd5ded8bbf1009ad7a767fe8ea4824696
Henry Chu <henry.chu013198@gmail.com>
2023-06-22 17:13:40 -0400

    editQuantity function in CartItem.js handles 0 or less new quantity and same quantity changes

--- frontend/src/components/CartItem/CartItem.js
+++ frontend/src/components/CartItem/CartItem.js
@@ -1,33 +1,44 @@
 import './CartItem.css'
 
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 // redux
-import { deleteCartItem } from '../../store/slices/cartSlice';
+import { addItemToCart, deleteCartItem } from '../../store/slices/cartSlice';
 
 const CartItem = ({cartItem}) => {
   const dispatch = useDispatch();
   const { cartItems } = useSelector(state => state.cart);
   const { user } = useSelector(state => state.auth);
+  const [ editing, setEditing ] = useState(false);
   const { product, size, quantity } = cartItem;
   const total = product.price * quantity;
-  
+  const quantityRef = useRef();
+  let email = "";
+  if(user) email = user.email;
+
   const deleteItem = () => {
-    let email = "";
-    if(user) email = user.email;
     dispatch(deleteCartItem({product, size, currentCart: cartItems, email}));
   };
 
   const editQuantity = () => {
-    console.log("editing item");
+    const newQuantity = quantityRef.current.value;
+    if(newQuantity <= 0){
+      dispatch(deleteCartItem({product, size, currentCart: cartItems, email}));
+    }
+    else if(newQuantity == quantity){
+      setEditing(false);
+    }
+    else{
+      console.log("last choice")
+    }
   };
 
   return (
     <div className="cart-item">
       <Link className="cart-item-image" to={"/product/" + product._id}>
-        <img src={product.imageURLs[0]}/>
+        <img src={product.imageURLs[0]} alt="loading"/>
       </Link>
       <div className="cart-item-main">
         <h3>{product.name}</h3>
@@ -35,14 +46,29 @@ const CartItem = ({cartItem}) => {
         <div className="cart-item-main-description">
           <p>Color: &nbsp;{product.color}</p>
           <p>Size: &nbsp;{size}</p>
-          <p>Quantity: &nbsp;{quantity}</p>
+          {editing &&
+            <div className="cart-item-editing-wrapper">
+              <p>Quantity: </p>
+              <input type="number" ref={quantityRef} default={quantity}/>
+              <p className="material-symbols-outlined cart-item-close-button"
+                  onClick={() => setEditing(false)}>close</p>
+              <p className="material-symbols-outlined cart-item-confirm-button"
+                  onClick={editQuantity}>check</p>
+            </div> 
+          }
+          {!editing && 
+            <p>Quantity: &nbsp;{quantity}</p>
+          }
           <p>Total: &nbsp;${total}</p>
         </div>
       </div>
-      <p className="material-symbols-outlined cart-item-delete-button" onClick={deleteItem}
-          >delete</p>
-      <p className="material-symbols-outlined cart-item-edit-button" onClick={editQuantity}
-          >edit</p>
+      <div className="cart-item-buttons">
+        <p className="material-symbols-outlined cart-item-edit-button" 
+            onClick={() => setEditing(true)}
+            >edit</p>
+        <p className="material-symbols-outlined cart-item-delete-button" onClick={deleteItem}
+            >delete</p>
+      </div>
     </div>
   )
 }
