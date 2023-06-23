ba92f66a96fdcee7aea6164050f2074e847f19fd
Henry Chu <henry.chu013198@gmail.com>
2023-06-22 23:02:31 -0400

    Created draft of order model

--- backend/src/order/orderModel.js
+++ backend/src/order/orderModel.js
@@ -1,2 +1,55 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+const orderSchema = new Schema({
+  items: [{
+    product: Object,
+    size: String,
+    quantity: Number
+  }],
+  email: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  addressLine2: {
+    type: String
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  postalCode: {
+    type: Number,
+    required: true
+  },
+  state: {
+    type: String,
+    required: true
+  },
+  subtotal: {
+    type: Number,
+    required: true
+  },
+  shipping: {
+    type: Number,
+    required: true
+  },
+  tax: {
+    type: Number,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Order', orderSchema);
